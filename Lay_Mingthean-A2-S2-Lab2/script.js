3383caf8cb8e0662aaa78744d14fa3adf6f93125
Thean <mingthean.lay@gmail.com>
2021-02-10 22:12:01 +0700

    add functionality

--- Lay_Mingthean-A2-S2-Lab2/script.js
+++ Lay_Mingthean-A2-S2-Lab2/script.js
@@ -5,26 +5,123 @@ const inputUsername = document.querySelector("[data-input-username]");
 const inputCoin = document.querySelector("[data-input-coin]");
 const allInput = document.querySelectorAll("input");
 const buttonRest = document.querySelector("[button-reset]");
+const displaySlot = document.querySelectorAll("[slot]");
+const buttonStart = document.querySelector("[button-start]");
+const displaySlotOne = document.querySelector("[slot-one]");
+const displaySlotTwo = document.querySelector("[slot-two]");
+const displaySlotThree = document.querySelector("[slot-three]");
+const betTiger = document.querySelector("[data-tiger]");
+const betLobster = document.querySelector("[data-lobster]");
+const betKlouk = document.querySelector("[data-klouk]");
+const betCrab = document.querySelector("[data-crab]");
+const betRoster = document.querySelector("[data-roster]");
+const betFish = document.querySelector("[data-fish]");
+const buttonClear = document.querySelector("[button-clear]");
+const betAmount = document.querySelectorAll("[data]");
 
+let slot = [];
+let slotOne = 1;
+let slotTwo = 1;
+let slotThree = 1;
 let coin = 0;
+let betMax = 0;
 let username = null;
+let prize = 0;
 
-init();
+function bet() {
+  slot.push(slotOne);
+  slot.push(slotTwo);
+  slot.push(slotThree);
+}
+
+function betCapacity() {
+  betAmount.forEach((input) => {
+    input.addEventListener("change", () => {
+      if (betMax === 2) {
+        lock();
+        alert("You can input only 3");
+        betMax = 0;
+      } else {
+        betMax++;
+      }
+    });
+  });
+}
+
+function lock() {
+  betAmount.forEach((input) => {
+    input.setAttribute("disabled", "disabled");
+  });
+}
+
+function unlock() {
+  betAmount.forEach((input) => {
+    input.removeAttribute("disabled");
+  });
+  clearBet();
+}
 
 function reset() {
+  init();
   allInput.forEach((input) => {
     input.value = "";
   });
+  coin = 0;
+  username = null;
+  slot = [];
 }
 
 function init() {
   displayCoin.innerText = coin;
   displayUsername.innerText = username;
+  if (coin === 0) {
+    inputCoin.removeAttribute("disabled");
+    inputUsername.removeAttribute("disabled");
+    buttonSubmit.removeAttribute("disabled");
+  } else {
+    inputCoin.setAttribute("disabled", "disabled");
+    inputUsername.setAttribute("disabled", "disabled");
+    buttonSubmit.setAttribute("disabled", "disabled");
+  }
+  unlock();
+  prize = 0;
+  slot = [];
 }
+
+function clearBet() {
+  betAmount.forEach((input) => {
+    input.value = "";
+  });
+}
+
+function random() {
+  if (username === null && coin === 0) {
+    alert("Input name and insert coin");
+    reset();
+    return;
+  } else {
+    slotOne = Math.floor(Math.random() * 6) + 1;
+    slotTwo = Math.floor(Math.random() * 6) + 1;
+    slotThree = Math.floor(Math.random() * 6) + 1;
+    displaySlotOne.src = "./img/" + slotOne + ".png";
+    displaySlotTwo.src = "./img/" + slotTwo + ".png";
+    displaySlotThree.src = "./img/" + slotThree + ".png";
+    bet();
+    init();
+  }
+}
+
+buttonStart.addEventListener("click", random);
+
+buttonClear.addEventListener("click", init);
+
+displaySlot.forEach((img) => {
+  img.src = "./img/1.png";
+});
 
 buttonSubmit.addEventListener("click", () => {
   if (!isNaN(inputCoin.value)) {
-    coin = inputCoin.value;
+    coin = parseInt(inputCoin.value);
     username = inputUsername.value;
     init();
   } else {
@@ -36,4 +133,8 @@ buttonSubmit.addEventListener("click", () => {
 
 buttonRest.addEventListener("click", () => {
   reset();
+  init();
 });
+
+init();
+betCapacity();
